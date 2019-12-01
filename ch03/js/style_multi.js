3656ff71742903212b7c879e406a61091c4d935f
uni51 <uni51@users.noreply.github.com>
2019-12-02 06:13:47 +0900

    finish 3-4-4

---
+++ ch03/js/style_multi.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#app',
+  data: {
+    // スタイル情報を準備
+    color: {
+      backgroundColor: 'Aqua',
+      color: 'Red'
+    },
+    size: {
+      fontSize: '1.5em'
+    }
+  }
+});
