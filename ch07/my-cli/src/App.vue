8ec4cd3ad35c7d5c78916ec20a80afab14dbd94b
uni51 <uni51@users.noreply.github.com>
2019-12-27 12:17:09 +0900

    Chapter7 VueCLI

---
+++ ch07/my-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>こんにちは、Vue.js</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style scoped>
+p {
+  border: 1px solid Red;
+  background-color: Yellow;
+}
+</style>
